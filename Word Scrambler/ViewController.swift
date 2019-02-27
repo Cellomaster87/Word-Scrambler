738e873d7d4a879370dae70ec8f6b1a76d4116b4
Michele Galvagno <michicello@gmail.com>
2019-02-27 22:10:42 +0100

    Prompt for Answer method
    - bar button item
    - alert controller + closure
    - submit placeholder method

--- Word Scrambler/ViewController.swift
+++ Word Scrambler/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    // MARK: Properties
     var allWords = [String]()
     var usedWords = [String]()
 
+    // MARK: - View management
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -28,12 +32,32 @@ class ViewController: UITableViewController {
         startGame()
     }
     
+    // MARK: - Methods
     func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
     
+    @objc func promptForAnswer() {
+        let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak alertController] _ in
+            guard let answer = alertController?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        alertController.addAction(submitAction)
+        present(alertController, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
+    }
+    
+    // MARK: - TableView Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }

b0cc347396627d63975382e51f5a3e3ed812e1d6
Michele Galvagno <michicello@gmail.com>
2019-02-27 20:28:42 +0100

    Initial Commit

---
+++ Word Scrambler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scrambler
+//
+//  Created by Michele Galvagno on 27/02/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
